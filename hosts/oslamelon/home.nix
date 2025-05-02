b2d7d430900699743732d52012e5a16ed3ba50d5
Shrek <91736051+ToAN7@users.noreply.github.com>
2025-05-02 15:26:04 +0700

    window + x to enable waybar and window + w to disable waybar. I may create a script specifically for toggle waybar

--- hosts/oslamelon/home.nix
+++ hosts/oslamelon/home.nix
@@ -14,6 +14,7 @@
         packages = with pkgs; [
             mpv
             fastfetch
+            vlc
 
             (writeShellScriptBin "my-hello" ''
                 echo "Hello, ${config.home.username}!"
