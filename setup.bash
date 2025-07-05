5d2f1a87f794623ac4ba6613f51f2dc2a560845d
oslamelon <91736051+ChauDucToan@users.noreply.github.com>
2025-07-05 10:34:16 +0700

    Add eval ssh-agent in setup.bash

--- setup.bash
+++ setup.bash
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 num=0;
-while :
-do 
+while true; do 
     echo "========SETUP========"
     echo "1. Build system configuration"
     echo "2. Build home configuration"
+    echo "3. Auto ssh eval"
     echo "====================="
 
-    read num
-    if [[ "$num" =~ ^[1-2] ]]; then
+    read -rp "Choose an option (1-3): " num
+    if [[ "$num" =~ ^[1-3] ]]; then
         break
     else 
         echo "Please choose the right option"
@@ -21,5 +21,17 @@ case $num in
         eval "sudo nixos-rebuild switch --flake .#$(whoami)";;
     "2")
         eval "home-manager switch --flake .#$(whoami)";;
+    "3")
+        eval "$(ssh-agent)"
+        while true; do
+            read -rp "Enter SSH key name (without .pub) in \$HOME: " p
+            if [[ -e "$HOME/.ssh/$p" && -e "$HOME/.ssh/$p.pub" ]]; then
+                ssh-add "$HOME/.ssh/$p"
+                break
+            else
+                echo "Key not found at $HOME/.ssh/$p — please try again"
+            fi
+        done
+        ;;
 esac
 
